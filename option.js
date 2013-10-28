3452845339745d1c1ecdad4b3765a1e949d9ca60
Brian McKenna <brian@brianmckenna.org>
2013-10-28 09:41:08 -0600

    Add OptionT

--- option.js
+++ option.js
@@ -67,6 +67,60 @@ Option.prototype.ap = function(a) {
     });
 };
 
+// Transformer
+Option.OptionT = function(M) {
+    var OptionT = daggy.tagged('run');
+    OptionT.prototype.fold = function(f, g) {
+        return this.run.chain(function(o) {
+            return M.of(o.fold(f, g));
+        });
+    };
+    OptionT.of = function(x) {
+        return OptionT(M.of(Option.Some(x)));
+    };
+    OptionT.prototype.orElse = function(b) {
+        return OptionT(this.run.chain(function(a) {
+            return a.fold(
+                function(x) {
+                    return M.of(a);
+                },
+                function() {
+                    return b.run;
+                }
+            );
+        }));
+    };
+    OptionT.prototype.getOrElse = function(x) {
+        return this.run.chain(function(o) {
+            return M.of(o.getOrElse(x));
+        });
+    };
+    OptionT.prototype.chain = function(f) {
+        var m = this.run;
+        return OptionT(m.chain(function(o) {
+            return o.fold(
+                function(a) {
+                    return f(a).run;
+                },
+                function() {
+                    return M.of(Option.None);
+                }
+            );
+        }));
+    };
+    OptionT.prototype.map = function(f) {
+        return this.chain(function(a) {
+            return OptionT.of(f(a));
+        });
+    };
+    OptionT.prototype.ap = function(a) {
+        return this.chain(function(f) {
+            return a.map(f);
+        });
+    };
+    return OptionT;
+};
+
 // Export
 if(typeof module != 'undefined')
     module.exports = Option;
